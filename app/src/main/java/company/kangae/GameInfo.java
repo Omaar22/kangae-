27433d3a61e883746bf71c27e9864841b70a212e
Maryam El-Sokkary <maryam.sokkary@gmail.com>
2016-12-21 21:46:43 +0200

    game list added, game info added

---
+++ app/src/main/java/company/kangae/GameInfo.java
@@ -0,0 +1,15 @@
+package company.kangae;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameInfo extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_info);
+    }
+
+
+}
