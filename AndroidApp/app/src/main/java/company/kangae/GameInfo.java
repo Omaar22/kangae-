4718db420103417e46ee8cf66ebe4525d1f99bd9
Maryam El-Sokkary <maryam.sokkary@gmail.com>
2016-12-21 22:46:13 +0200

    restructuring again

--- AndroidApp/app/src/main/java/company/kangae/GameInfo.java
+++ AndroidApp/app/src/main/java/company/kangae/GameInfo.java
@@ -1,10 +1,20 @@
 package company.kangae;
 
+import android.content.Context;
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class GameInfo extends AppCompatActivity {
 
@@ -21,7 +31,8 @@ public class GameInfo extends AppCompatActivity {
         TextView instructions = (TextView) findViewById(R.id.instructions);
         TextView rating = (TextView) findViewById(R.id.rating);
         TextView difficultyRank = (TextView) findViewById(R.id.difficultyrank);
-        ListView comments = (ListView) findViewById(R.id.comments);
+        ListView commentsList = (ListView) findViewById(R.id.comments);
+        Button playGame = (Button) findViewById(R.id.playGame);
 
         name.setText(game.getName());
         category.setText(game.getCategory());
@@ -29,11 +40,42 @@ public class GameInfo extends AppCompatActivity {
         instructions.setText(game.getInstructions());
         rating.setText(game.getRate());
         difficultyRank.setText(Integer.toString(game.getDifficultyRank()));
+        ArrayList <Comment> comments = game.getComments();
+
+        ArrayAdapter arrayAdapter = new GameInfo.commentAdapter(this, comments);
+        commentsList.setAdapter(arrayAdapter);
+
+        final Context context = this;
+
+
 
 
 
     }
 
+    class commentAdapter extends ArrayAdapter<Comment> {
+
+        public commentAdapter(Context context, ArrayList<Comment> comments) {
+            super(context, R.layout.comments_list, comments);
+        }
+
+        @NonNull
+        @Override
+        public View getView(int position, View convertView, @NonNull ViewGroup parent) {
+
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            View view = inflater.inflate(R.layout.comments_list, parent, false);
+            TextView userName = (TextView) view.findViewById(R.id.userName);
+            TextView comment = (TextView) view.findViewById(R.id.comment);
+
+
+            Comment newComment = getItem(position);
+            userName.setText(newComment.getUserName());
+            comment.setText(newComment.getComment());
+
+            return view;
+        }
+    }
 
 
 
--- AndroidApp/app/src/main/java/company/kangae/MainActivity.java
+++ AndroidApp/app/src/main/java/company/kangae/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Button logIn = (Button) findViewById(R.id.logInButton);
         logIn.setOnClickListener(new View.OnClickListener() {
                                      @Override
