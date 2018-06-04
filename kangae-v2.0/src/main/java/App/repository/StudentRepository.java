2d752a9ff2284d306622ab90a36d77347bff2aa6
Omaar22 <o-mohamed@hotmail.com>
2018-06-05 01:10:34 +0200

    Refactor

--- kangae-v2.0/src/main/java/App/repository/StudentRepository.java
+++
@@ -1,7 +0,0 @@
-package App.repository;
-
-import App.model.Student;
-
-public interface StudentRepository extends UserBaseRepository<Student> {
-
-}
--- kangae-v2.0/src/main/java/App/repository/TeacherRepository.java
+++
@@ -1,6 +0,0 @@
-package App.repository;
-
-import App.model.Teacher;
-
-public interface TeacherRepository extends UserBaseRepository<Teacher>{
-}
